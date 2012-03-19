39fff50c3f0d1e24bef033d32362d8283a5bc088
Grupo-3XP <mcalaguap@gmail.com>
2012-03-19 15:30:30 -0500

    Creacion del proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConcultaDatosApp::Application.config.secret_token = '956b89cbd358693b519996d9bc85feb1cd83cafcc7c814a873267027d4494c866dca47cfd22d00b3d06d3e296b2abf986f5c52293eabde42e45fc0b36f3b300a'
